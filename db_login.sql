66d235b92ff055b4c1a670921169e81c7e71555c
connornclark <connor.n.clark@gmail.com>
2018-02-22 23:17:23 -0500

    added database

--- db_login.sql
+++ db_login.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Feb 16, 2018 at 12:24 AM
+-- Generation Time: Feb 23, 2018 at 04:16 AM
 -- Server version: 5.7.19
 -- PHP Version: 5.6.31
 
@@ -32,23 +32,23 @@ DROP TABLE IF EXISTS `tbl_user`;
 CREATE TABLE IF NOT EXISTS `tbl_user` (
   `user_id` smallint(4) UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_fname` varchar(50) NOT NULL,
-  `user_lname` varchar(50) NOT NULL,
   `user_name` varchar(50) NOT NULL,
   `user_pass` varchar(50) NOT NULL,
-  `user_level` varchar(25) NOT NULL,
+  `user_email` varchar(200) NOT NULL,
+  `user_lvllist` varchar(25) NOT NULL,
   `user_ip` varchar(100) NOT NULL,
   `user_timestamp` varchar(100) NOT NULL,
-  `user_loginAttempts` tinyint(4) NOT NULL,
-  `user_lastLoginAttempt` datetime NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_user`
 --
 
-INSERT INTO `tbl_user` (`user_id`, `user_fname`, `user_lname`, `user_name`, `user_pass`, `user_level`, `user_ip`, `user_timestamp`, `user_loginAttempts`, `user_lastLoginAttempt`) VALUES
-(1, 'Connor', 'Clark', 'connorclark', 'connor1234', '1', '::1', 'Thursday February 15th 2018, at 07:23pm EST', 0, '0000-00-00 00:00:00');
+INSERT INTO `tbl_user` (`user_id`, `user_fname`, `user_name`, `user_pass`, `user_email`, `user_lvllist`, `user_ip`, `user_timestamp`) VALUES
+(1, 'Connor', 'connorclark', 'connor1234', '', '1', '::1', 'Thursday February 22nd 2018, at 07:34pm EST'),
+(13, 'tom', 'tom1234', 'tom4321', 'sdfsdf', '2', 'no', 'no'),
+(14, 'sdfsdf', 'sdfsdf', 'sdfsdf', 'sdfsdf', '2', 'no', 'no');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
